054a12aff017860cdf59e6bf72664c6fc8cce5c9
Julián Rojas <juliandra140201@gmail.com>
2023-01-28 23:11:23 -0500

    Complete configuration of app_binding.dart

--- lib/src/app/app_binding.dart
+++ lib/src/app/app_binding.dart
@@ -6,7 +6,8 @@ class AppBinding implements Bindings {
   const AppBinding();
   @override
   void dependencies() {
-    //Inyectamos las dependencias
+    //Inyectamos las dependencias (va a permanecer así durante toda la ejecución)
+    //Indicamos esto con permanent: true
     Get.put<AuthController>(AuthController(), permanent: true);
     Get.put<AppController>(AppController(), permanent: true);
   }
