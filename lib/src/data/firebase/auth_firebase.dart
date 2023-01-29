5f566c3192985321a0c646d8049acebdb2e2d4dc
Julián Rojas <juliandra140201@gmail.com>
2023-01-28 22:58:00 -0500

    Complete configuration of auth_firebase.dart

---
+++ lib/src/data/firebase/auth_firebase.dart
@@ -0,0 +1,16 @@
+import 'package:prototipo_app_biblioteca_flutter/src/domain/repositories/auth_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthFirebase implements AuthRepository {
+  const AuthFirebase();
+  @override
+  Future<UserCredential> signInWithEmail(
+    final String username,
+    final String password,
+  ) {
+    return FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: username,
+      password: password,
+    );
+  }
+}
