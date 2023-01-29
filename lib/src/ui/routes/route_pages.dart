448f4f07f1285331562e9a697d67fc2f0af877aa
Julián Rojas <juliandra140201@gmail.com>
2023-01-28 23:27:50 -0500

    Complete configuration of route_pages.dart

---
+++ lib/src/ui/routes/route_pages.dart
@@ -0,0 +1,32 @@
+import 'package:prototipo_app_biblioteca/src/ui/pages/home/home_binding.dart';
+import 'package:prototipo_app_biblioteca/src/ui/pages/home/home_page.dart';
+import 'package:prototipo_app_biblioteca/src/ui/pages/sign_in/sign_in_binding.dart';
+import 'package:prototipo_app_biblioteca/src/ui/pages/sign_in/sign_in_page.dart';
+import 'package:prototipo_app_biblioteca/src/ui/pages/splash/splash_binding.dart';
+import 'package:prototipo_app_biblioteca/src/ui/pages/splash/splash_page.dart';
+import 'package:prototipo_app_biblioteca/src/ui/routes/route_names.dart';
+import 'package:get/get.dart';
+
+class RoutePages {
+  const RoutePages._();
+
+  static List<GetPage<dynamic>> get all {
+    return [
+      GetPage(
+        name: RouteNames.home,
+        page: () => const HomePage(),
+        binding: const HomeBinding(),
+      ),
+      GetPage(
+        name: RouteNames.signIn,
+        page: () => const SignInPage(),
+        binding: const SignInBinding(),
+      ),
+      GetPage(
+        name: RouteNames.splash,
+        page: () => const SplashPage(),
+        binding: const SplashBinding(),
+      ),
+    ];
+  }
+}
