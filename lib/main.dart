65b5a4e516cc3e8da6b886bbafc59f38870a1156
Julián Rojas <juliandra140201@gmail.com>
2023-01-28 22:44:21 -0500

    Import firebase_options.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:prototipo_app_biblioteca_flutter/src/app/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaulFirebaseOptions.currentPlataform);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
 }
