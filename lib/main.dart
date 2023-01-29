cb45de920e349429c5b1795b6bb488ae7e45a0a7
Julián Rojas <juliandra140201@gmail.com>
2023-01-28 23:50:44 -0500

    Complete configuration of main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:prototipo_app_biblioteca_flutter/src/app/app.dart';
+import 'package:prototipo_app_biblioteca_flutter/src/app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:prototipo_app_biblioteca_flutter/src/app/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
