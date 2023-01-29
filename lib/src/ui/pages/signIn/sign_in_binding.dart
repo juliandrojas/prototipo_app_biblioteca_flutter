97c1b8aa5c11c3f62e32f8b46e636bbc0f5a22bf
Julián Rojas <juliandra140201@gmail.com>
2023-01-28 23:40:13 -0500

    Complete configuration of sign_in_binding.dart

---
+++ lib/src/ui/pages/signIn/sign_in_binding.dart
@@ -0,0 +1,11 @@
+import 'package:prototipo_app_biblioteca/src/ui/pages/signIn/sign_in_controller.dart';
+import 'package:get/get.dart';
+
+class SignInBinding implements Bindings {
+  const SignInBinding();
+
+  @override
+  void dependencies() {
+    Get.lazyPut<SignInController>(() => SignInController());
+  }
+}
